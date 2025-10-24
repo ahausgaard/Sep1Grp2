527211ee59e0d5738e8f8037caa8ffa551b7439f
Andreas Jakobsen <ahausgaardjakobsen@gmail.com>
2025-10-24 11:36:05 +0200

    Co-authored-by: AnasYusufDev <AnasYusufDev@users.noreply.github.com> Co-authored-by: Nasteco12 <Nasteco12@users.noreply.github.com> Co-authored-by: TarikMaarouf <TarikMaarouf@users.noreply.github.com>

--- Web1_prj2_AccountCreation/script.js
+++ Web1_prj2_AccountCreation/script.js
@@ -2,26 +2,30 @@ let currentStep = 0;
 const steps = document.querySelectorAll(".step");
 
 function showStep(index) {
-    steps.forEach((step, i) => {
-        step.computedStyleMap.display = i === index ? "block" : "none";
-    });
+  steps.forEach((step, i) => {
+    step.style.display = i === index ? "block" : "none";
+  });
+}
 
-function nextStep(){
-    if (currentStep < steps.length - 1 ) {
-        currentStep++;
-        showStep(currentStep);
-    }
+function nextStep() {
+  if (currentStep < steps.length - 1) {
+    currentStep++;
+    showStep(currentStep);
+  }
+}
 
 function prevStep() {
-    if (currentStep > 0) {
-        currentStep--;
-        showStep(currentStep);
-    }
+  if (currentStep > 0) {
+    currentStep--;
+    showStep(currentStep);
+  }
+}
 
-document.getElementById("multiStepForm").addEventListener("submit", function(e) {
+document.addEventListener("DOMContentLoaded", () => {
+  showStep(currentStep);
+
+  document.getElementById("multiStepForm").addEventListener("submit", function (e) {
     e.preventDefault();
     alert("Form submitted!");
-})
-}
-}
-}
+  });
+});
