8c9da664d21be0d823a8b1e7db770bc23b55c303
TarikMaarouf <tarikmaarouf7@gmail.com>
2025-12-04 09:26:23 +0100

    Hej
    med ig

--- Kløverly/src/kløverly/presentation/controllers/AddTaskController.java
+++ Kløverly/src/kløverly/presentation/controllers/AddTaskController.java
@@ -1,9 +1,6 @@
 package kløverly.presentation.controllers;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import kløverly.domain.CommunityTask;
 import kløverly.domain.Task;
 import kløverly.persistence.DataManager;
@@ -11,45 +8,70 @@ import kløverly.presentation.core.ControllerConfigurator;
 
 public class AddTaskController
 {
-  public Label statusLabel;
-  public Button addTaskButton;
-  public Button cancelButton;
-  public TextField taskNameInput;
-  public TextField taskDescriptionInput;
-  public Slider taskValueSlider;
-  private DataManager dm;
+    public Label statusLabel;
+    public Button addTaskButton;
+    public Button cancelButton;
+    public TextField taskNameInput;
+    public TextField taskDescriptionInput;
+    public Slider taskValueSlider;
+    public Label sliderInput;
+    private DataManager dm;
+    public ComboBox<String> choiceBoxDrop;
 
-  public void initialize()
-  {
-    dm = ControllerConfigurator.getDataManager();
-  }
+    public void initialize()
+    {
+        dm = ControllerConfigurator.getDataManager();
 
-  public void onAddTaskButtonPressed()
-  {
-    //Take input and save in variables
-    String name = taskNameInput.getText();
-    String description = taskDescriptionInput.getText();
-    int value = (int) taskValueSlider.getValue();
 
-    //New task. Still hardcoded CommunityTask. WIP
-    Task newTask = new CommunityTask(name, "Fælles", value, description);
-    dm.addTask(newTask);
+        //TODO Lav i FX
+        choiceBoxDrop.getItems().addAll("Bytteopgave", "FællesOpgave", "item 3");
 
-    //Reset inputs
-    taskNameInput.setText("");
-    taskDescriptionInput.setText("");
-    taskValueSlider.setValue(0);
+        taskValueSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
 
-    System.out.println(dm.toString());
+            sliderInput.setText(Double.toString(newValue.intValue()));
 
-  }
 
-  public void onCancelButtonPressed()
-  {
+        });
 
-  }
+    }
+    //PRØVER IGEN GITHUYB
+    public void onAddTaskButtonPressed()
+    {
+        //Take input and save in variables
+        String name = taskNameInput.getText();
+        String description = taskDescriptionInput.getText();
+        int value = (int) taskValueSlider.getValue();
+
+
+        String type = choiceBoxDrop.getValue();
+
+
+
+        Task newTask = new CommunityTask(name, type, value, description);
+        dm.addTask(newTask);
+
+        taskNameInput.setText("Test");
+
+
+        //Reset inputs
+        taskNameInput.setText("");
+
+        //TODO Lav i fx (slider)
+        taskDescriptionInput.setText("");
+        taskValueSlider.setValue(0);
+
+
+        System.out.println(dm.toString());
+
+    }
+
+    public void onCancelButtonPressed()
+    {
+
+    }
 
 
 
 
 }
+
